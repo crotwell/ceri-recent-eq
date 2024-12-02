9f7711b0c6b7ac25d890926a7001017c85295cc7
Philip Crotwell <crotwell@seis.sc.edu>
2024-12-02 14:33:56 -0500

    move endsAfter to now (Dec 2024)

--- public/data/load_recent.sh
+++ public/data/load_recent.sh
@@ -2,4 +2,4 @@
 
 curl -o recent_events.json 'http://folkworm.ceri.memphis.edu/REQ/json/recent_events.json?map_type=recent'
 
-curl -o stations.staxml 'https://service.iris.edu/fdsnws/station/1/query?net=US,OK,O2,SE,NM,N4,KY,LD,IU,IM,ET,CO,AG&endafter=2024-05-01T00:00:00&cha=BH?,HH?,SH?&level=channel&format=xml&maxlat=42.800&minlon=-101.646&maxlon=-64.644&minlat=23.711&includecomments=false&nodata=404'
+curl -o stations.staxml 'https://service.iris.edu/fdsnws/station/1/query?net=US,OK,O2,SE,NM,N4,KY,LD,IU,IM,ET,CO,AG&endafter=2024-12-01T00:00:00&cha=BH?,HH?,SH?&level=channel&format=xml&maxlat=42.800&minlon=-101.646&maxlon=-64.644&minlat=23.711&includecomments=false&nodata=404'
